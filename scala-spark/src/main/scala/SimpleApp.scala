4ce541357ae9c6d30b63ff17644736ba03459181
wangjc1 <like_dark@126.com>
2018-07-13 22:05:20 +0800

    spark simple demo

--- scala-spark/src/main/scala/SimpleApp.scala
+++ scala-spark/src/main/scala/SimpleApp.scala
@@ -7,14 +7,19 @@ import org.apache.spark.{SparkContext, SparkConf}
  */
 object SimpleApp {
   def main(args: Array[String]) {
-    val file = Thread.currentThread().getContextClassLoader().getResource("test.txt").getPath
+    //输入文件既可以是本地linux系统文件，也可以是其它来源文件，例如HDFS
+    val logFile = Thread.currentThread().getContextClassLoader().getResource("test.txt").getPath
 
-    val logFile = "test.txt" // Should be some file on your system
-    val conf = new SparkConf().setAppName("Simple Application").setMaster("local[2]")
+    //以本地线程方式运行，可以指定线程个数，
+    //如.setMaster("local[2]")，两个线程执行
+    //下面给出的是单线程执行
+    val conf = new SparkConf().setAppName("SparkWordCount").setMaster("local[2]")
     val sc = new SparkContext(conf)
-    val logData = sc.textFile(file, 2).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+
+    //wordcount操作，计算文件中包含Spark的行数
+    val count=sc.textFile(logFile).filter(line => line.contains("Spark")).count()
+    //打印结果
+    println("count="+count)
+    sc.stop()
   }
 }
